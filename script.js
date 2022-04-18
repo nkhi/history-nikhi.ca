0a792b6c8c67d92c3650584daf7fe572cd073f78
nikhi <nikhibhambra@gmail.com>
2022-04-18 00:05:17 -0400

    prettify

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var letterDelete = 0;
 var visible = true;
 
 // constant blinking cursor
-window.setInterval(function () {
+window.setInterval(function() {
     if (visible) {
         terminal.className = 'console-underscore hidden';
         visible = false;
@@ -19,54 +19,54 @@ window.setInterval(function () {
 
 // write and delete words
 function terminalText(word) {
-	let docLoc = document.getElementById('text');
+    let docLoc = document.getElementById('text');
 
-	window.setTimeout(function () {
+    window.setTimeout(function() {
 
-		// write out
-		window.setTimeout(function () {
-			if (letterCount === 0) {
-				while (letterCount <= word.length) {
-					(function (letterCount) {
-						window.setTimeout(function () {
-							if (letterCount === 1) {
-								terminal.innerHTML = "&#95;";
-							}
-							docLoc.innerHTML = word.substring(0, letterCount);
-						}, 100 * (letterCount));
-					})(letterCount++);
-				}
-			}
-		}, 0);
+        // write out
+        window.setTimeout(function() {
+            if (letterCount === 0) {
+                while (letterCount <= word.length) {
+                    (function(letterCount) {
+                        window.setTimeout(function() {
+                            if (letterCount === 1) {
+                                terminal.innerHTML = "&#95;";
+                            }
+                            docLoc.innerHTML = word.substring(0, letterCount);
+                        }, 100 * (letterCount));
+                    })(letterCount++);
+                }
+            }
+        }, 0);
 
-		// backspace, but only for the first word
-		window.setTimeout(function () {
-			if (letterCount >= word.length && word != word2) {
-				while (letterCount >= 0 && letterDelete != word.length + 1) {
-					(function (letterCount) {
-						window.setTimeout(function () {
-							if (letterCount < 1) {
-								terminal.innerHTML = "&nbsp;&#95;";
-							}
-							docLoc.innerHTML = word.substring(0, letterCount);
-						}, 1000 - (letterCount*40));
-					})(letterDelete++);
-				}
-			}
-		}, 2000);
+        // backspace, but only for the first word
+        window.setTimeout(function() {
+            if (letterCount >= word.length && word != word2) {
+                while (letterCount >= 0 && letterDelete != word.length + 1) {
+                    (function(letterCount) {
+                        window.setTimeout(function() {
+                            if (letterCount < 1) {
+                                terminal.innerHTML = "&nbsp;&#95;";
+                            }
+                            docLoc.innerHTML = word.substring(0, letterCount);
+                        }, 1000 - (letterCount * 40));
+                    })(letterDelete++);
+                }
+            }
+        }, 2000);
 
-		letterCount = 0;
-		letterDelete = 0;
+        letterCount = 0;
+        letterDelete = 0;
 
-	}, 1500);
+    }, 1500);
 }
 
 // first executes writing typo after 1.5 seconds
 terminalText(word1);
 
 // second executes writing 'progress' after 3 seconds
-window.setTimeout(function () {
-	terminalText(word2);
+window.setTimeout(function() {
+    terminalText(word2);
 }, 3000);
 
 // Adapted from indubitablee on SO
@@ -82,17 +82,23 @@ function ClipBoard() {
 }
 
 const targetElement = document.querySelector("html");
-const payload = {element: targetElement}
+const payload = {
+    element: targetElement
+}
 setTimeout(() => {
-	switch(Math.floor(Math.random()*6)) {
-		case 0: case 2: case 4:
-			new fairyDustCursor(payload);
-			break;
-		case 1: case 3: case 5:
-			new snowflakeCursor(payload);
-			break;
+    switch (Math.floor(Math.random() * 6)) {
+        case 0:
+        case 2:
+        case 4:
+            new fairyDustCursor(payload);
+            break;
+        case 1:
+        case 3:
+        case 5:
+            new snowflakeCursor(payload);
+            break;
         default:
             console.log("unlucky. try refreshing the page :)");
             break;
-	}
-} , 5000);
+    }
+}, 5000);
