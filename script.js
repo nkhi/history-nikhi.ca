53e5e95a666f334962d35e5483e0d0e918f56a36
Nikhi Bhambra <nikhibhambra@gmail.com>
2022-12-04 22:20:33 -0500

    Hide audio button for now

--- script.js
+++ script.js
@@ -498,28 +498,28 @@ function showAbout() {
   }, 2000);
 
   // show new icons
-  if (document.getElementById("options-container") == null) {
-    document.getElementById(
-      "top-part"
-    ).innerHTML += `<div id='options-container' class='options'>
-        <span id='button' class='first-option' title='Turn on narration' onmouseenter={tintBackgroundTo('251101')}>
-          <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
-        </span>
-      </div>`;
-    // document.getElementById("top-part").innerHTML +=
-    //   "<div id='options-container' class='options'><span id='button' class='first-option' title='Turn on narration' onmouseenter={tintBackgroundTo('251101')}><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg></span></div>";
-  } else {
-    document.getElementById("options-container").classList.remove("hidden");
-  }
+  // if (document.getElementById("options-container") == null) {
+  //   document.getElementById(
+  //     "top-part"
+  //   ).innerHTML += `<div id='options-container' class='options'>
+  //       <span id='button' class='first-option' title='Turn on narration' onmouseenter={tintBackgroundTo('251101')}>
+  //         <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>
+  //       </span>
+  //     </div>`;
+  //   // document.getElementById("top-part").innerHTML +=
+  //   //   "<div id='options-container' class='options'><span id='button' class='first-option' title='Turn on narration' onmouseenter={tintBackgroundTo('251101')}><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg></span></div>";
+  // } else {
+  //   document.getElementById("options-container").classList.remove("hidden");
+  // }
 
-  document
-    .getElementById("options-container")
-    .addEventListener("mouseleave", () => {
-      document.documentElement.style.background = "#0c0c0d";
-    });
-  document.getElementById("button").addEventListener("click", () => {
-    turnOnNarration();
-  });
+  // document
+  //   .getElementById("options-container")
+  //   .addEventListener("mouseleave", () => {
+  //     document.documentElement.style.background = "#0c0c0d";
+  //   });
+  // document.getElementById("button").addEventListener("click", () => {
+  //   turnOnNarration();
+  // });
 
   let desc = document.getElementById("desc");
   // TODO write an about section
