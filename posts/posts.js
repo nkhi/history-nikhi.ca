53f06fc193ee973d881703b1da59b2b44c91580a
Nikhi Bhambra <nikhibhambra@gmail.com>
2023-01-08 11:46:08 -0500

    Adds a play button

--- posts/posts.js
+++ posts/posts.js
@@ -23,33 +23,36 @@ async function openPost(postId) {
     importJson(jason);
 }
 
+function pauseNarration() {
+    let narrationAudio = document.getElementById("audio-player");
+    narrationAudio.pause();
+    document.getElementById("play-icon").outerHTML = "<svg id='play-icon' onClick={playNarration()} title='Play audio narration' xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-play-circle'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>";
+    document.getElementById("play-icon").setAttribute("onclick", "playNarration()");
+}
+
+// make this accept a param to dynamically handle path routing
+// make this change icons
 function playNarration() {
-    return
-}
-
-// 
-function progressBarScroll() {
-    return
-}
-
-function matchContentHeight() {
-    return
+    let narrationAudio = document.getElementById("audio-player");
+    narrationAudio.play();
+    document.getElementById("play-icon").outerHTML = "<svg id='play-icon' onClick={playNarration()} title='Play audio narration'  xmlns='http://www.w3.org/2000/svg' width='28' height='28' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-pause-circle'><circle cx='12' cy='12' r='10'></circle><line x1='10' y1='15' x2='10' y2='9'></line><line x1='14' y1='15' x2='14' y2='9'></line></svg>";
+    document.getElementById("play-icon").setAttribute("onclick", "pauseNarration()");
 }
 
 function expandMeta() {
     document.getElementById('heading-container').classList.remove('hidden');
     document.getElementById("content").style.maxHeight="calc(100vh - 3rem - 230px)";
-    document.getElementById("play-icon").classList.remove('hidden');
-    document.getElementById("title").style.paddingLeft = '10px';
+    document.getElementById("play-icon").title="Play audio narration";
     meta.onclick=shrinkMeta;
     matchMetaElementBeforeHeight();
 }
 
+// Give this a gradient or something randomly chosen on page enter
+// use css vars
 function shrinkMeta() {
     document.getElementById('heading-container').classList.add('hidden');
     document.getElementById("content").style.maxHeight="calc(100vh - 3rem - 142px)";
-    document.getElementById("play-icon").classList.add('hidden');
-    document.getElementById("title").style.paddingLeft = "42px"
+    document.getElementById("play-icon").title="Pause audio narration";
     meta.onclick=expandMeta;
     matchMetaElementBeforeHeight();
 }
@@ -62,6 +65,16 @@ function matchMetaElementBeforeHeight() {
     matchContentHeight();
 }
   
+function progressBarScroll() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+  }
+
+function matchContentHeight() {
+    return
+}
 
 // make method to break json on br tags with <p> wrappers for a11y
 
@@ -73,7 +86,7 @@ openPost(6);
 
 window.setTimeout(function () {
     shrinkMeta();
-}, 10000);
+}, 6000);
 
 // lol
 window.setTimeout(() => {
@@ -94,3 +107,5 @@ meta.addEventListener("mouseenter", () => {
 meta.addEventListener("mouseleave", () => {
     meta.style.setProperty('--metaBackground', 'rgb(91, 8, 158)');
 });
+
+document.getElementById('audio-player').volume = 0.5;
