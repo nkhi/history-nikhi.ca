dd074003e9ca744c37d75234b5dfdbbb2ce85f05
Nikhi Bhambra <nikhibhambra@gmail.com>
2023-01-08 10:58:33 -0500

    Add meta accordion & fix title sash css
    Working on progress bar

--- posts/posts.js
+++ posts/posts.js
@@ -2,6 +2,8 @@ let idToFilenameIndex = {
     '6':'06_AI_death_and_centrality_of_meaning.json'
 }
 
+let meta = document.getElementById("meta")
+
 // open txt file and show
 function importJson(json) {
     var postTitle = document.getElementById("title");
@@ -25,6 +27,42 @@ function playNarration() {
     return
 }
 
+// 
+function progressBarScroll() {
+    return
+}
+
+function matchContentHeight() {
+    return
+}
+
+function expandMeta() {
+    document.getElementById('heading-container').classList.remove('hidden');
+    document.getElementById("content").style.maxHeight="calc(100vh - 3rem - 230px)";
+    document.getElementById("play-icon").classList.remove('hidden');
+    document.getElementById("title").style.paddingLeft = '10px';
+    meta.onclick=shrinkMeta;
+    matchMetaElementBeforeHeight();
+}
+
+function shrinkMeta() {
+    document.getElementById('heading-container').classList.add('hidden');
+    document.getElementById("content").style.maxHeight="calc(100vh - 3rem - 142px)";
+    document.getElementById("play-icon").classList.add('hidden');
+    document.getElementById("title").style.paddingLeft = "42px"
+    meta.onclick=expandMeta;
+    matchMetaElementBeforeHeight();
+}
+
+// use this to fix the meta:before elements height
+function matchMetaElementBeforeHeight() {
+    let meta = document.getElementById("meta");
+    let meta_height = meta.offsetHeight + "px";
+    meta.style.setProperty('--metaBeforeElementHeight',meta_height);
+    matchContentHeight();
+}
+  
+
 // make method to break json on br tags with <p> wrappers for a11y
 
 // method to close / open top container like an accordion
@@ -33,7 +71,26 @@ openPost(6);
 
 // Shrink title after 10s
 
-// window.setTimeout(function () {
-//     document.getElementById('heading-container').classList.add('hidden');
-//     document.getElementById('meta::before');
-//   }, 10000);
+window.setTimeout(function () {
+    shrinkMeta();
+}, 10000);
+
+// lol
+window.setTimeout(() => {
+    matchMetaElementBeforeHeight();
+}, 200);
+
+window.onscroll = function () {
+    progressBarScroll();
+};
+
+// keep meta:before height the same as the meta tag itself.
+// recurring and onPageLoad.
+window.addEventListener("resize", matchMetaElementBeforeHeight);
+meta.addEventListener("mouseenter", () => {
+    meta.style.setProperty('--metaBackground', 'rgb(68, 5, 120)');
+});
+
+meta.addEventListener("mouseleave", () => {
+    meta.style.setProperty('--metaBackground', 'rgb(91, 8, 158)');
+});
